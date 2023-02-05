b00b99630927a02f55b07ca5d111fc900ea40b7b
Espch <ddudorkin@gmail.com>
2023-02-06 00:56:17 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,131 +1,96 @@
-﻿namespace ConsoleApp3
+﻿namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Всего актав 4 , чтобы поменять актаву нажмите F2 или нажмите Escape , чтобы закончить сессию");
-            ConsoleKeyInfo start = Console.ReadKey();
-            while (start.Key != ConsoleKey.Escape)
-            {
-                if (start.Key == ConsoleKey.F5)
+            string a = " ";
+            while(a!= "Выход") { 
+                Console.WriteLine("Меню");
+                Console.WriteLine(" 1. Угадай число");
+                Console.WriteLine(" 2. Таблица умножения");
+                Console.WriteLine(" 3. Вывод делителей числа");
+                Console.WriteLine("Напишите 'Выход' если хотите закончить сессию ");
+                Console.Write("Выберите действие : ");
+                a = Console.ReadLine();
+                if(a == "1")
                 {
-                    Console.WriteLine("F5");
-                    while (start.Key != ConsoleKey.F4 && start.Key != ConsoleKey.F3 && start.Key != ConsoleKey.F2 && start.Key != ConsoleKey.Escape)
-                    {
-                        start = key(start, 0);
-                    }
-                }
-                else if (start.Key == ConsoleKey.F2)
+                    Random();
+                }else if(a == "2")
                 {
-                    Console.WriteLine("F2");
-                    while (start.Key != ConsoleKey.F5 && start.Key != ConsoleKey.F3 && start.Key != ConsoleKey.F4 && start.Key != ConsoleKey.Escape)
-                    {
-                        start = key(start, 2);
-                    }
-                }
-                else if (start.Key == ConsoleKey.F3)
+                    TY();
+                }else if(a == "3")
                 {
-                    Console.WriteLine("F3");
-                    while (start.Key != ConsoleKey.F2 && start.Key != ConsoleKey.F5 && start.Key != ConsoleKey.F4 && start.Key != ConsoleKey.Escape)
-                    {
-                        start = key(start, 3);
-                    }
-                }
-                else if (start.Key == ConsoleKey.F4)
+                    delitel();
+                }else if(a == "Выход")
                 {
-                    Console.WriteLine("F4");
-                    while (start.Key != ConsoleKey.F2 && start.Key != ConsoleKey.F3 && start.Key != ConsoleKey.F5 && start.Key != ConsoleKey.Escape)
-                    {
-                        start = key(start, 1);
-                    }
+                    Console.WriteLine("Досвидания");
                 }
                 else
                 {
-                    Console.WriteLine(" Такой актавы нету");
-                    start = Console.ReadKey();
+                    Console.WriteLine("Такого действия в списке нету попробуйте еще раз");
                 }
             }
-        }
+         }
 
-        static int sound(int i, int j) // функция массива частоты
+        static void Random()
         {
-            int[,] arr = new int[4, 12] { { 523, 587, 659, 698, 784, 880, 987, 554, 622, 740, 830, 932, }, { 261, 293, 329, 349, 392, 440, 494, 277, 311, 370, 415, 366, }, { 65, 73, 82, 87, 98, 110, 123, 69, 78, 93, 103, 116 }, { 130, 146, 164, 174, 196, 220, 246, 138, 155, 185, 207, 233 } };
-            return arr[i, j];
+            Console.WriteLine(" ");
+            Random rand = new Random();
+
+            Console.Write("Угадайте число от 0 до 100 !!! : ");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            int a = rand.Next(0, 100);
+
+            while(i != a)
+            {
+                Console.Write("Ой вы не угадали попройте еще раз ");
+                i = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Поздарвляю вы угадали !!!!!!");
+            Console.WriteLine(" ");
         }
-        static ConsoleKeyInfo key(ConsoleKeyInfo start, int i) // функция клавиш
+        static void TY()
         {
-            start = Console.ReadKey();
-            if (start.Key == ConsoleKey.Q) //до
+            Console.WriteLine(" ");
+            var arr = new int[10, 8];
+
+            int N = 1;
+
+            for (int i = 0; i < 9; i++)
             {
-                int N = sound(i, 0);
-                sound2(N);
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i == 0)
+                    {
+                        N++;
+                        arr[i, j] = N;
+                    }
+                    else
+                    {
+                        arr[i, j] = (i+1) * arr[0, j];
+                    }
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine(" ");
             }
-            else if (start.Key == ConsoleKey.W) // ре
-            {
-                int N = sound(i, 1);
-                sound2(N);
-            }
-            else if (start.Key == ConsoleKey.E) // ми
-            {
-                int N = sound(i, 2);
-                sound2(N);
-            }
-            else if (start.Key == ConsoleKey.R) // фа
-            {
-                int N = sound(i, 3);
-                sound2(N);
-            }
-            else if (start.Key == ConsoleKey.T) // соль
-            {
-                int N = sound(i, 4);
-                sound2(N);
-            }
-            else if (start.Key == ConsoleKey.Y) // ля
-            {
-                int N = sound(i, 5);
-                sound2(N);
-            }
-            else if (start.Key == ConsoleKey.U) // си
-            {
-                int N = sound(i, 6);
-                sound2(N);
-            }
-            else if (start.Key == ConsoleKey.D2)
-            {
-                int N = sound(i, 7);
-                sound2(N);
-            }
-            else if (start.Key == ConsoleKey.D3)
-            {
-                int N = sound(i, 8);
-                sound2(N);
-            }
-            else if (start.Key == ConsoleKey.D5)
-            {
-                int N = sound(i, 9);
-                sound2(N);
-            }
-            else if (start.Key == ConsoleKey.D6)
-            {
-                int N = sound(i, 10);
-                sound2(N);
-            }
-            else if (start.Key == ConsoleKey.D7)
-            {
-                int N = sound(i, 11);
-                sound2(N);
-            }
-            else if (start.Key != ConsoleKey.F2 && start.Key != ConsoleKey.F3 && start.Key != ConsoleKey.F5 && start.Key != ConsoleKey.F4 && start.Key != ConsoleKey.Escape)
-            {
-                Console.WriteLine("Такой клавиши нету");
-            }
-            return start;
+            Console.WriteLine(" ");
         }
-        public static void sound2(int N)
+        static void delitel()
         {
-            Console.Clear();
-            Console.Beep(N, 200);
+            Console.WriteLine(" ");
+            Console.Write("Введите число : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i < a + 1;i++)
+            {
+                if(a % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine(" ");
         }
     }
 }
